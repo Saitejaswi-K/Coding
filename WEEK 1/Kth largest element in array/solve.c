0b41d0afcf66be85ae8e85d12217e10d7dae5a39
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2022-12-23 19:43:40 +0530

    Kth largest element in array

--- WEEK 1/Kth largest element in array/solve.c
+++ WEEK 1/Kth largest element in array/solve.c
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
+    int findKthLargest(vector<int>& nums, int k){
         priority_queue <int> pq;
         for(int i=0; i<nums.size(); i++){
             pq.push(nums[i]);
